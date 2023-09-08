741d32e54a8590c7b1f8bd19a6f00cefe8469490
misc0001 <misc0001@stud.kea.dk>
2023-09-08 13:29:58 +0200

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+    String a = " Hej";
+    String b = " Nicklas";
+    String c = " Mazlum";
+    String d = "!";
+
+    System.out.println(a + b + c + d);
+    System.out.println(c + b + a + d);
+    }
+}
+git 
